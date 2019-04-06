9163e6d4612efe69992dee552141c7aec1f3b3be
xpttxsok123123 <jay.xu@wework.com>
2019-04-06 16:31:17 +0800

    xpttxsOK123123@123123

---
+++ src/main/java/com/xp/controller/DetailController.java
@@ -0,0 +1,23 @@
+package com.xp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @ClassName DetailController
+ * @Description TODO
+ * @Author
+ * @Date 2019/4/6 16:29
+ **/
+public class DetailController {
+
+
+    public String index(){
+        return "index";
+    }
+
+
+    public String indexq(){
+        return "index";
+    }
+
+}
