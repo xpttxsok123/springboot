0880cd0458a675235c90f02a731bf691ea49b930
xpttxsok123123 <jay.xu@wework.com>
2019-04-06 17:01:40 +0800

    xpttxsOK123123@123123

--- src/main/java/com/xp/controller/IndexController.java
+++ src/main/java/com/xp/controller/IndexController.java
@@ -5,18 +5,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author xupan
+ * @date 2019/04/06
  */
 @Controller
 public class IndexController {
 
+    /**
+     * 首页跳转
+     *
+     * @return 返回字符串
+     */
     @RequestMapping(value = "/index")
-    public String index(){
+    public String index() {
         return "index";
     }
 
 
+    /**
+     * @param index 下标
+     * @return 返回字符
+     */
     @RequestMapping(value = "/index1")
-    public String indexq(){
+    public String indexq(String index) {
         return "index";
     }
 }
