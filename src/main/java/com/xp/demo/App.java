2c48149b129c98f8b18dcc5dd499cdb1925ebe1b
jay.xu@wework.com <jay.xu@wework.com>
2020-01-01 18:12:35 +0800

    delete files

--- src/main/java/com/xp/demo/App.java
+++ src/main/java/com/xp/demo/App.java
@@ -1,9 +1,12 @@
 package com.xp.demo;
 
+import java.util.HashMap;
+
 public class App {
 
+    // 1，2，3，4
     public static void main(String[] args) {
-        System.out.println(1);
+      
     }
 
 }
